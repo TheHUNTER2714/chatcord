e3bd2a9b2d460190aa902c14b065f2052b39a0ab
Ayush Agnihotri <ayushagnihotri964@gmail.com>
2025-05-08 14:46:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+const path = require("path");
 const express = require("express");
 const app = express();
+app.use(express.static(path.join(__dirname, "public")));
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, { 
   cors: { 
